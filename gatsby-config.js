384a81f25824e23bc2d7e8afffa2259eb8d63c70
Fabien Le Frapper <contact@fabienlefrapper.me>
2017-11-28 22:10:26 +0100

    Add styled components to gatsby list of plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Interférence - Radio Mega`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
