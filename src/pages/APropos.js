2f2fa13fa8c8736cbcf6dba25b3aec73cb41d1a9
Fabien Le Frapper <contact@fabienlefrapper.me>
2018-01-09 23:11:55 +0100

    Retours Raf

--- src/pages/APropos.js
+++ src/pages/APropos.js
@@ -9,6 +9,13 @@ const P = styled.p`
     column-count: 1;
   }
 `
+const Pp = styled.p`
+  text-align: center;
+  font-size: .9em;
+  a {
+    color: rgb(179, 75, 36);
+  }
+`
 
 const APropos = () => (
   <Section
@@ -18,16 +25,18 @@ const APropos = () => (
   >
     <P>
 			« Interférences » la pièce radiophonique est une adaptation radio de la bande déssinée. Piloté par le scénariste Laurent Galandon et confié au bon soin d’une radio associative, Radio Méga basée à Valence et qui émet depuis plus de 35 ans. L’enregistrement de cette pièce radiophonique a rassemblé plus d’une quinzaine d’acteurs et s’est enregistré dans de nombreux lieux à Valence.
+		</P>
+		<Pp>
+      <br /><br />
+			une fiction de Laurent Galandon,<br />
+			mise en scène Ingrid Lebrasseur,<br />
+			Mixage -Réalisation Raphaël Terribile,<br />
+			prise de sons, Nicolas Crouan Raphaël Terribile,<br />
+			Chargé de production, Julien Marty, Eric Baral<br />
+			Une production Dargaud - Radio Méga.<br />
 
-			une fiction de Laurent Galandon,
-			mise en scène Ingrid Lebrasseur,
-			Mixage -Réalisation Raphaël Terribile,
-			prise de sons, Nicolas Crouan Raphaël Terribile,
-			Chargé de production, Julien Marty, Eric Baral
-			Une production Dargaud - Radio Méga.
-
-			pour toutes demandes de diffusions et d’interviews : raphael.terribile@radio-mega.com
-    </P>
+			pour toutes demandes de diffusions et d’interviews : <a href="mailto:raphael.terribile@radio-mega.com" title="Raphael Terribile email">raphael.terribile@radio-mega.com</a><br />
+    </Pp>
   </Section>
 )
 
